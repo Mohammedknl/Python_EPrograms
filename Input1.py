d0960baa09155eff53f2cbb17988c38d39495634
Mohammedknl <125635185+Mohammedknl@users.noreply.github.com>
2023-04-02 19:00:29 +0530

    Add files via upload

---
+++ Input1.py
@@ -0,0 +1,14 @@
+#This is a single line comment in Python
+'''
+This is
+a Multi line comment
+in Python
+'''
+'''Write a Program to accept a number from the user and 
+display double of that number in the O/P.'''
+# Ask the user for a number
+num = int(input("Enter a number: "))
+# Double the number
+double_num = num * 2
+# Print the result using the comma operator
+print("The double of", num, "is", double_num)
